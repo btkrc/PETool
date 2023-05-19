e89cd0aba71fe504b8446b18c21ffa453da1e5a2
btkrc <33425848@qq.com>
2023-05-19 18:22:02 +0800

    init

---
+++ PETool.cpp
@@ -0,0 +1,333 @@
+#pragma once
+#include"pch.h"
+#include"PETool.h"
+
+DWORD __stdcall PEAlign(DWORD value, DWORD size) {
+    if (value <= size) {
+        return size;
+    }
+    if (value > size && !(value % size)) {
+        return value;
+    }
+    if (value > size && (value % size)) {
+        return value + (size - (value % size));
+    }
+
+    return NULL;
+}
+
+LPVOID __stdcall createFileBuffer(LPCTSTR fileName) {
+    FILE *fp;
+
+    errno_t err = fopen_s(&fp, fileName, "rb");
+
+    if (err) {
+        printf("err %d\n", err);
+        return NULL;
+    }
+
+    if (!fp) {
+        printf("fopen error\n");
+        return NULL;
+    }
+
+    /*获取文件大小*/
+    fseek(fp, 0, SEEK_END);
+    DWORD size = ftell(fp);
+
+    LPVOID lpFileBuffer = malloc(size);
+
+    if (!lpFileBuffer) {
+        printf("申请内存失败..\n");
+        fclose(fp);
+        return NULL;
+    }
+
+    fseek(fp, 0, SEEK_SET);
+
+    fread_s(lpFileBuffer, size, size, 1, fp);
+
+    fclose(fp);
+
+    return lpFileBuffer;
+}
+
+LPVOID __stdcall file2Image(LPVOID lpFileBuffer, DWORD *pdwBufferSize) {
+    PIMAGE_DOS_HEADER pDos =
+        (PIMAGE_DOS_HEADER)lpFileBuffer;
+    PIMAGE_NT_HEADERS pNT =
+        (PIMAGE_NT_HEADERS)((DWORD)lpFileBuffer + pDos->e_lfanew);
+    PIMAGE_FILE_HEADER pFile =
+        (PIMAGE_FILE_HEADER)((DWORD)pNT + 4);
+    PIMAGE_OPTIONAL_HEADER pOption =
+        (PIMAGE_OPTIONAL_HEADER)((DWORD)pFile + IMAGE_SIZEOF_FILE_HEADER);
+    PIMAGE_SECTION_HEADER pFirstSection =
+        PIMAGE_SECTION_HEADER((DWORD)pOption + pFile->SizeOfOptionalHeader);
+
+    /*按照拉伸后的大小申请内存*/
+    LPVOID lpImageBuffer = malloc(pOption->SizeOfImage);
+
+    memset(lpImageBuffer, 0, pOption->SizeOfImage);
+
+    /*写入头部，不需要对齐，下面从VA开始写入*/
+    memcpy(lpImageBuffer, lpFileBuffer, pOption->SizeOfHeaders);
+
+    /*遍历写入节区，按照内存对齐写入*/
+    PIMAGE_SECTION_HEADER pSectionHeader = pFirstSection;
+    for (int i = 0; i < pFile->NumberOfSections; i++) {
+        memcpy(
+            (LPVOID)((DWORD)lpImageBuffer + pSectionHeader->VirtualAddress),
+            (LPVOID)((DWORD)lpFileBuffer + pSectionHeader->PointerToRawData),
+            pSectionHeader->Misc.VirtualSize
+        );
+        /*指向下一个节表*/
+        pSectionHeader = (PIMAGE_SECTION_HEADER)((DWORD)pSectionHeader + IMAGE_SIZEOF_SECTION_HEADER);
+    }
+
+    *pdwBufferSize = pOption->SizeOfImage;
+
+
+    return lpImageBuffer;
+}
+
+LPVOID __stdcall image2File(LPVOID lpImageBuffer, DWORD *pdwBufferSize) {
+    PIMAGE_DOS_HEADER pDos =
+        (PIMAGE_DOS_HEADER)lpImageBuffer;
+    PIMAGE_NT_HEADERS pNT =
+        (PIMAGE_NT_HEADERS)((DWORD)lpImageBuffer + pDos->e_lfanew);
+    PIMAGE_FILE_HEADER pFile =
+        (PIMAGE_FILE_HEADER)((DWORD)pNT + 4);
+    PIMAGE_OPTIONAL_HEADER pOption =
+        (PIMAGE_OPTIONAL_HEADER)((DWORD)pFile + IMAGE_SIZEOF_FILE_HEADER);
+    PIMAGE_SECTION_HEADER pFirstSection =
+        PIMAGE_SECTION_HEADER((DWORD)pOption + pFile->SizeOfOptionalHeader);
+
+    /*获取最后一个节表的地址*/
+    PIMAGE_SECTION_HEADER pLastSection =
+        PIMAGE_SECTION_HEADER((DWORD)pFirstSection + ((pFile->NumberOfSections - 1) * IMAGE_SIZEOF_SECTION_HEADER));
+
+    /*获取FileBuffer的大小，用最后一个节的文件偏移加上文件对齐大小得出*/
+    *pdwBufferSize = pLastSection->PointerToRawData + pLastSection->SizeOfRawData;
+
+    /*按照文件对齐大小分配内存*/
+    LPVOID lpFileBuffer = malloc(*pdwBufferSize);
+
+    memset(lpFileBuffer, 0, *pdwBufferSize);
+
+    /*写入头，按照文件对齐*/
+    memcpy(lpFileBuffer, lpImageBuffer, pOption->SizeOfHeaders);
+
+    PIMAGE_SECTION_HEADER pSectionHeader = pFirstSection;
+    for (int i = 0; i < pFile->NumberOfSections; i++) {
+        /*按照文件对齐遍历写入*/
+        memcpy(
+            (LPVOID)((DWORD)lpFileBuffer + pSectionHeader->PointerToRawData),
+            (LPVOID)((DWORD)lpImageBuffer + pSectionHeader->VirtualAddress),
+            pSectionHeader->SizeOfRawData
+        );
+
+        /*指向下一个节表*/
+        pSectionHeader = (PIMAGE_SECTION_HEADER)((DWORD)pSectionHeader + IMAGE_SIZEOF_SECTION_HEADER);
+    }
+
+    return lpFileBuffer;
+}
+
+
+LPVOID __stdcall mergeSection(LPVOID lpImageBuffer) {
+    PIMAGE_DOS_HEADER pDos =
+        (PIMAGE_DOS_HEADER)lpImageBuffer;
+    PIMAGE_NT_HEADERS pNT =
+        (PIMAGE_NT_HEADERS)((DWORD)lpImageBuffer + pDos->e_lfanew);
+    PIMAGE_FILE_HEADER pFile =
+        (PIMAGE_FILE_HEADER)((DWORD)pNT + 4);
+    PIMAGE_OPTIONAL_HEADER pOption =
+        (PIMAGE_OPTIONAL_HEADER)((DWORD)pFile + IMAGE_SIZEOF_FILE_HEADER);
+    PIMAGE_SECTION_HEADER pFirstSection =
+        (PIMAGE_SECTION_HEADER)((DWORD)pOption + pFile->SizeOfOptionalHeader);
+
+    /*获取头内存对齐后的大小*/
+    DWORD alignHeaderSize = PEAlign(pOption->SizeOfHeaders, pOption->SectionAlignment);
+    /*获取最后一个节*/
+    PIMAGE_SECTION_HEADER pLastSection =
+        PIMAGE_SECTION_HEADER((DWORD)pFirstSection + ((pFile->NumberOfSections - 1) * IMAGE_SIZEOF_SECTION_HEADER));
+
+    /*计算最后一个节的大小中更大的一个*/
+    DWORD max = pLastSection->SizeOfRawData < pLastSection->Misc.VirtualSize ? pLastSection->Misc.VirtualSize : pLastSection->SizeOfRawData;
+
+    /*计算整个节区的大小*/
+    pFirstSection->SizeOfRawData = pFirstSection->Misc.VirtualSize = (DWORD)pLastSection->VirtualAddress + max - alignHeaderSize;
+
+    pFirstSection->PointerToRawData = pFirstSection->VirtualAddress;
+    /*遍历修改第一个节表的属性 包含所有节表的属性*/
+    PIMAGE_SECTION_HEADER pSectionHeader = pFirstSection;
+    for (int i = 0; i < pFile->NumberOfSections; i++) {
+        pFirstSection->Characteristics |= pSectionHeader->Characteristics;
+        pFirstSection = (PIMAGE_SECTION_HEADER)((DWORD)pFirstSection + 40);
+    }
+
+    /*将节表的数量置1*/
+    pFile->NumberOfSections = 1;
+
+    return lpImageBuffer;
+
+}
+
+
+DWORD RVA2FOA(LPVOID lpBuffer, DWORD RVA) {
+    PIMAGE_DOS_HEADER pDos =
+        (PIMAGE_DOS_HEADER)lpBuffer;
+    PIMAGE_NT_HEADERS pNT =
+        (PIMAGE_NT_HEADERS)((DWORD)lpBuffer + pDos->e_lfanew);
+    PIMAGE_FILE_HEADER pFile =
+        (PIMAGE_FILE_HEADER)((DWORD)pNT + 4);
+    PIMAGE_OPTIONAL_HEADER pOption =
+        (PIMAGE_OPTIONAL_HEADER)((DWORD)pFile + IMAGE_SIZEOF_FILE_HEADER);
+    PIMAGE_SECTION_HEADER pFirstSection =
+        PIMAGE_SECTION_HEADER((DWORD)pOption + pFile->SizeOfOptionalHeader);
+
+    DWORD FOA;
+
+    /*对齐相同时不需转换*/
+    if (pOption->FileAlignment == pOption->SectionAlignment) {
+        return RVA;
+    }
+
+    /*判断RVA是否处于头中*/
+    if (RVA < pOption->SizeOfHeaders || pOption->FileAlignment == pOption->SectionAlignment) {
+        return RVA;
+    }
+
+    PIMAGE_SECTION_HEADER pSectionHeader = pFirstSection;
+
+    /*判断RVA处于哪一个节区*/
+    for (int i = 0; i < pFile->NumberOfSections; i++) {
+        if (RVA >= pSectionHeader->VirtualAddress &&
+            RVA < pSectionHeader->VirtualAddress + pSectionHeader->Misc.VirtualSize) {
+            /*获取RVA和节区VA的偏移*/
+            DWORD relSectionImageAdd = RVA - pSectionHeader->VirtualAddress;
+            /*偏移加上节区文件地址得到FOA*/
+            FOA = relSectionImageAdd + pSectionHeader->PointerToRawData;
+            return FOA;
+        }
+
+        /*指向下一个节表*/
+        pSectionHeader = (PIMAGE_SECTION_HEADER)((DWORD)pSectionHeader + IMAGE_SIZEOF_SECTION_HEADER);
+    }
+
+    return NULL;
+
+}
+
+DWORD FOA2RVA(LPVOID lpBuffer, DWORD FOA) {
+    PIMAGE_DOS_HEADER pDos =
+        (PIMAGE_DOS_HEADER)lpBuffer;
+    PIMAGE_NT_HEADERS pNT =
+        (PIMAGE_NT_HEADERS)((DWORD)lpBuffer + pDos->e_lfanew);
+    PIMAGE_FILE_HEADER pFile =
+        (PIMAGE_FILE_HEADER)((DWORD)pNT + 4);
+    PIMAGE_OPTIONAL_HEADER pOption =
+        (PIMAGE_OPTIONAL_HEADER)((DWORD)pFile + IMAGE_SIZEOF_FILE_HEADER);
+    PIMAGE_SECTION_HEADER pFirstSection =
+        PIMAGE_SECTION_HEADER((DWORD)pOption + pFile->SizeOfOptionalHeader);
+
+    DWORD RVA;
+
+    if (pOption->FileAlignment == pOption->SectionAlignment) {
+        return FOA;
+    }
+
+    /*判断FOA是否处于头中*/
+    if (FOA < pOption->SizeOfHeaders || pOption->FileAlignment == pOption->SectionAlignment) {
+        return FOA;
+    }
+
+    PIMAGE_SECTION_HEADER pSectionHeader = pFirstSection;
+
+    for (int i = 0; pFile->NumberOfSections; i++) {
+        if (FOA >= pSectionHeader->PointerToRawData &&
+            FOA < pSectionHeader->PointerToRawData + pSectionHeader->SizeOfRawData) {
+            /*获取FOA和节区文件地址的偏移*/
+            DWORD relSectionFileAdd = FOA - pSectionHeader->PointerToRawData;
+            /*偏移加节区的VA得到RVA*/
+            RVA = relSectionFileAdd + pSectionHeader->VirtualAddress;
+            return RVA;
+        }
+        /*指向下一个节表*/
+        pSectionHeader = (PIMAGE_SECTION_HEADER)((DWORD)pSectionHeader + IMAGE_SIZEOF_SECTION_HEADER);
+    }
+
+    return NULL;
+}
+
+
+DWORD getFunctionAddressByName(LPVOID lpFileBuffer, LPCTSTR checkName) {
+    PIMAGE_DOS_HEADER pDos =
+        (PIMAGE_DOS_HEADER)lpFileBuffer;
+    PIMAGE_NT_HEADERS pNT =
+        (PIMAGE_NT_HEADERS)((DWORD)lpFileBuffer + pDos->e_lfanew);
+    PIMAGE_FILE_HEADER pFile =
+        (PIMAGE_FILE_HEADER)((DWORD)pNT + 4);
+    PIMAGE_OPTIONAL_HEADER pOption =
+        (PIMAGE_OPTIONAL_HEADER)((DWORD)pFile + IMAGE_SIZEOF_FILE_HEADER);
+    PIMAGE_SECTION_HEADER pFirstSection =
+        (PIMAGE_SECTION_HEADER)((DWORD)pOption + pFile->SizeOfOptionalHeader);
+
+    PIMAGE_EXPORT_DIRECTORY pExportDir =
+        (PIMAGE_EXPORT_DIRECTORY)(RVA2FOA(lpFileBuffer, pOption->DataDirectory[0].VirtualAddress) + (DWORD)lpFileBuffer);
+
+    /*获取函数地址表、函数名称表、函数序号表的地址*/
+    PDWORD lpAddOfFns = (PDWORD)(RVA2FOA(lpFileBuffer, (DWORD)pExportDir->AddressOfFunctions) + (DWORD)lpFileBuffer);
+    PDWORD lpAddOfNames = (PDWORD)(RVA2FOA(lpFileBuffer, (DWORD)pExportDir->AddressOfNames) + (DWORD)lpFileBuffer);
+    /*序号表的数据为2个字节*/
+    PWORD lpAddOfOrdinals = (PWORD)(RVA2FOA(lpFileBuffer, (DWORD)pExportDir->AddressOfNameOrdinals) + (DWORD)lpFileBuffer);
+
+    DWORD FnAddr = 0;
+
+    for (int i = 0; i < pExportDir->NumberOfNames; i++) {
+        LPCTSTR fnName = (LPCTSTR)(RVA2FOA(lpFileBuffer, lpAddOfNames[i]) + (DWORD)lpFileBuffer);
+        if (!strcmp(fnName, checkName)) {
+            /*找到名字后通过下标继续索引序号表*/
+            DWORD fnOrdinal = lpAddOfOrdinals[i];
+            FnAddr = lpAddOfFns[fnOrdinal];
+            return FnAddr;
+        }
+    }
+    /*未找到，返回无效地址*/
+    return 0;
+}
+
+DWORD getFunctionAddressByOrdinal(LPVOID lpFileBuffer, DWORD checkOrdinal) {
+    PIMAGE_DOS_HEADER pDos =
+        (PIMAGE_DOS_HEADER)lpFileBuffer;
+    PIMAGE_NT_HEADERS pNT =
+        (PIMAGE_NT_HEADERS)((DWORD)lpFileBuffer + pDos->e_lfanew);
+    PIMAGE_FILE_HEADER pFile =
+        (PIMAGE_FILE_HEADER)((DWORD)pNT + 4);
+    PIMAGE_OPTIONAL_HEADER pOption =
+        (PIMAGE_OPTIONAL_HEADER)((DWORD)pFile + IMAGE_SIZEOF_FILE_HEADER);
+    PIMAGE_SECTION_HEADER pFirstSection =
+        (PIMAGE_SECTION_HEADER)((DWORD)pOption + pFile->SizeOfOptionalHeader);
+
+    PIMAGE_EXPORT_DIRECTORY pExportDir =
+        (PIMAGE_EXPORT_DIRECTORY)(RVA2FOA(lpFileBuffer, pOption->DataDirectory[0].VirtualAddress) + (DWORD)lpFileBuffer);
+
+    /*获取函数地址表、函数名称表、函数序号表的地址*/
+    PDWORD lpAddOfFns = (PDWORD)(RVA2FOA(lpFileBuffer, (DWORD)pExportDir->AddressOfFunctions) + (DWORD)lpFileBuffer);
+    PDWORD lpAddOfNames = (PDWORD)(RVA2FOA(lpFileBuffer, (DWORD)pExportDir->AddressOfNames) + (DWORD)lpFileBuffer);
+    /*序号表的数据为2个字节*/
+    PWORD lpAddOfOrdinals = (PWORD)(RVA2FOA(lpFileBuffer, (DWORD)pExportDir->AddressOfNameOrdinals) + (DWORD)lpFileBuffer);
+
+    /*导出序号 - Base得到序号表中的序号（地址表下标）*/
+    DWORD ordinal = checkOrdinal - pExportDir->Base;
+
+    /*序号不在地址表下标范围内*/
+    if (ordinal >= pExportDir->NumberOfFunctions || ordinal < 0) {
+        return 0;
+    }
+
+    DWORD fnAddr = lpAddOfFns[ordinal];
+
+    return fnAddr;
+
+}
---
+++ PETool.h
@@ -0,0 +1,23 @@
+#pragma once
+#include<windows.h>
+#include<iostream>
+#include<stdlib.h>
+
+/*对齐*/
+DWORD __stdcall PEAlign(DWORD value, DWORD size);
+/*读取PE文件*/
+LPVOID __stdcall createFileBuffer(LPCTSTR fileName);
+/*创建拉伸PE文件*/
+LPVOID __stdcall file2Image(LPVOID lpFileBuffer, DWORD *pdwBufferSize);
+/*将拉伸后的PE文件复原*/
+LPVOID __stdcall image2File(LPVOID lpImageBuffer, DWORD *pdwBufferSize);
+/*合并节*/
+LPVOID __stdcall mergeSection(LPVOID lpImageBuffer);
+/*内存偏移转文件偏移*/
+DWORD RVA2FOA(LPVOID lpBuffer, DWORD RVA);
+/*文件偏移转内存偏移*/
+DWORD FOA2RVA(LPVOID lpBuffer, DWORD FOA);
+/*通过函数名获取函数地址*/
+DWORD getFunctionAddressByName(LPVOID lpFileBuffer, LPCTSTR checkName);
+/*通过导出序号获取函数地址*/
+DWORD getFunctionAddressByOrdinal(LPVOID lpFileBuffer, DWORD checkOrdinal);
